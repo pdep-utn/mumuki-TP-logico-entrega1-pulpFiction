991400379ba8a9fe22509c3dfa18569175180863
occhiuzzi.santiago@gmail.com <occhiuzzi.santiago@gmail.com>
2016-06-21 23:42:14 -0400

    Mumuki Export on 2016-06-21 23:42:14 -0400

--- 00003_Nuevos trabajadores/extra.pl
+++ 00003_Nuevos trabajadores/extra.pl
@@ -1,7 +1,8 @@
+pareja(bernardo,    bianca).
+pareja(bernardo,    charo).
+
 saleCon(Uno, Otro):-pareja(Uno, Otro).
 saleCon(Otro, Uno):-pareja(Uno, Otro).
 
-pareja(bernardo,    bianca).
-pareja(bernardo,    charo).
 
 
