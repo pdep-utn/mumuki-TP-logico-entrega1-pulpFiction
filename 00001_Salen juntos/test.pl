9414706a5558eee0eb2218b98e7ef14b99696aec
gastonprietoutn@gmail.com <gastonprietoutn@gmail.com>
2016-06-26 11:54:25 -0400

    Mumuki Export on 2016-06-26 11:54:25 -0400

--- 00001_Salen juntos/test.pl
+++ 00001_Salen juntos/test.pl
@@ -1,8 +1,8 @@
-test('marsellus sale con mia') :-
-  saleCon(marsellus, Alguien), Alguien = mia.
+test('marsellus y mia salen juntos') :-
+  saleCon(marsellus, mia), saleCon(mia, marsellus).
   
-test('mia sale con marsellus') :-
-  saleCon(mia, Alguien), Alguien = marsellus.
+test('marsellus no sale con honeyBunny'):-
+  not(saleCon(marsellus, honeyBunny)).
   
 test('dos personas que salen entre ellas cumplen salir en ambos sentidos', [nondet]) :-
   saleCon(Alguien, Otro),
